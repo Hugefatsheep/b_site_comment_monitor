1ca1d672ea64b71207728534dc3f87e129f9ada6
Hugefatsheep <37077682+Hugefatsheep@users.noreply.github.com>
2020-09-20 09:29:27 +0800

    Initial commit

---
+++ GetIp.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'FatSheep'
+__time__ = '2020/9/19'
+"""
+# 快代理IP爬取 并建立可用IP池
+import requests
+import random
+import time
+from lxml import etree
+from fake_useragent import UserAgent
+
+ua = UserAgent(use_cache_server=False)
+headers = {'User-Agent': ua.chrome}
+http_proxies = []  # 全IP容器
+uesful_ip_pool = []  # 有用IP容器
+uesless_ip_pool = []  # 无用IP容器
+with open("proxy.txt", "r") as f:
+    all_ip = f.readlines()
+
+
+def get_new_ip():
+    proxy_ip = random.choice(all_ip)[:-1]
+    new_ip = {'HTTP': proxy_ip}
+    return new_ip
+
+
+# proxy={'http':'IP:PORT'}
+def ip_spider():  # 填写需要爬取IP的页数
+    n = int(input('请输入需要爬取的页数：'))
+
+    for a in range(1, n + 1):
+        proxy = get_new_ip()
+        url = f'https://www.kuaidaili.com/free/inha/{a}/'
+        try:
+            response = requests.get(url, headers=headers, proxies=proxy)
+        except ConnectionError:
+            print("connect error!")
+            break
+        except TimeoutError:
+            print("connect error!")
+            break
+        except requests.exceptions.ConnectionError:
+            print("connect error!")
+            break
+        # 判断请求状态
+        if response.status_code == 200:
+            html_str = response.text
+            re_tree = etree.HTML(html_str)
+            ips_list = re_tree.xpath('//td[@data-title="IP"]/text()')
+            types_list = re_tree.xpath('//td[@data-title="类型"]/text()')
+            ports_list = re_tree.xpath('//td[@data-title="PORT"]/text()')
+
+            print(ips_list)
+            print(types_list)
+            print(ports_list)
+            for i in list(zip(types_list, ips_list, ports_list)):
+                ip_port = i[1] + ':' + i[2]
+                ip_dic = {i[0]: ip_port}
+                http_proxies.append(ip_dic)
+        if a % 5 == 0:
+            time.sleep(5)
+        print(a)
+
+
+def check_ip():  # 定义IP检测
+    for proxy in http_proxies:
+        url = 'https://www.baidu.com/'
+        response = requests.get(url, headers=headers, proxies=proxy)
+        if response.status_code == 200:
+            uesful_ip_pool.append(proxy)
+        else:
+            uesless_ip_pool.append(proxy)
+
+
+ip_spider()
+check_ip()
+with open("proxy_2.txt", "w+") as fp:
+    for ip_address in uesful_ip_pool:
+        fp.write(ip_address["HTTP"] + "\n")
+
+print(f'有{len(uesful_ip_pool)}个可用IP，可用IP池列表为\n{uesful_ip_pool}')
+print('**************')
+print(f'有{len(uesless_ip_pool)}个无用IP，无用IP池列表为\n{uesless_ip_pool}')
---
+++ b_site_spider.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+__title__ = ''
+__author__ = 'FatSheep'
+__time__ = '2020/9/19'
+"""
+import requests
+import random
+import os
+import json
+import time
+from fake_useragent import UserAgent
+
+# https://api.bilibili.com/x/web-interface/newlist?rid=121&type=1&pn=5&ps=248900 分区视频BV号
+# B站API详情 https://github.com/Vespa314/bilibili-api/blob/master/api.md
+
+# B站分区tID
+subarea_list = [24, 25, 47, 86, 27, 13, 33, 32, 51, 152, 167, 153, 168, 169, 195, 170, 3, 28, 31, 30, 194, 59, 193,
+                29, 130, 129, 20, 198, 199, 200, 154, 156, 4, 17, 171, 172, 65, 173, 121, 136, 19, 36, 201, 124, 207,
+                208, 209, 122, 188, 95, 189, 190, 191, 160, 138, 21, 76, 75, 161, 162, 163, 176, 174, 119, 22, 26, 126,
+                127, 155, 157, 158, 164, 159, 192, 202, 203, 204, 205, 206, 5, 71, 137, 131, 181, 182, 183, 85, 184,
+                177, 37, 178]
+
+# 视频AV/BV号列表
+aid_list = []
+bvid_list = []
+
+# 评论用户及其信息
+info_list = []
+
+# 获取失败AV号
+failed_list = []
+
+dir_name = ""
+
+useragent = UserAgent(use_cache_server=False)
+headers = {'User-Agent': useragent.chrome}
+with open("proxy.txt", "r") as f:
+    all_ip = f.readlines()
+
+
+# 获取指定分区的所有视频的BV号 rid:分区编号 size:单次拉取数目 page:页数
+def getAllAVList(rid, day):  # page_start, page_end, size
+    # 获取分区视频列表
+    proxy = get_new_ip()  # 获取代理ip
+    url = "http://api.bilibili.com/x/web-interface/ranking/region?rid=" + str(rid) + "&day=" + str(day)
+    text = requests.get(url, proxies=proxy, headers=headers).text
+    json_text = json.loads(text)
+    # 遍历JSON格式信息，获取视频aid
+    if json_text["code"] == 0:
+        for item in json_text["data"]:
+            aid_list.append(item["aid"])
+            bvid_list.append(item["bvid"])
+    else:
+        print("Be banned!!!!!")
+        print(str(rid))
+        return 0
+    return 0
+
+
+# 获取一个AV号视频下所有评论
+def getAllCommentList(item):
+    proxy = get_new_ip()  # 获取代理ip
+    url = "http://api.bilibili.com/x/web-interface/view?aid=" + str(item)
+    numtext = requests.get(url, proxies=proxy, headers=headers).text
+    json_text = json.loads(numtext)
+    commentsNum = json_text["data"]["stat"]["reply"]
+    page = commentsNum // 20 + 1
+    for n in range(1, page):
+        proxy = get_new_ip()
+        url = "http://api.bilibili.com/x/v2/reply?type=1&oid=" + str(item) + "&pn=" + str(n)
+        text = requests.get(url, proxies=proxy, headers=headers).text
+        json_text_list = json.loads(text)
+        try:
+            for i in json_text_list["data"]["replies"]:
+                info_list.append([i["member"]["mid"], i["content"]["message"]])
+        except TypeError:
+            failed_list.append(item)
+            print("评论访问被拒绝")
+            return 0
+        if n % 5 == 0:
+            time.sleep(2.2)
+    return 1
+    # print(info_list)
+
+
+# 保存评论文件为txt
+def saveTxt(filename, filecontent):
+    if filecontent:
+        filename = str(filename) + ".txt"
+        with open(dir_name + "/" + filename, "w+", encoding='utf-8') as fp:
+            for content in filecontent:
+                if content[0] != '':
+                    fp.write(content[0] + ": " + content[1] + "\n")
+        print("文件" + str(filename) + "写入完毕")
+    else:
+        print(str(filename) + "无评论。")
+        return 0
+
+
+def get_new_ip():
+    proxy_ip = random.choice(all_ip)[:-1]
+    new_ip = {'HTTP': proxy_ip}
+    return new_ip
+
+
+if __name__ == "__main__":
+    for code in range(0, len(subarea_list)-1):
+        getAllAVList(subarea_list[code], 3)  # rid,page_start,page_end,size
+        dir_name = "./" + str(subarea_list[code]) + "_"
+        os.makedirs(dir_name)
+        count = 0
+        for item in aid_list:
+            info_list.clear()
+            getAllCommentList(item)
+            saveTxt(bvid_list[count], info_list)
+            count += 1
+            time.sleep(1)
+        with open(dir_name + "/failed.txt", "w+", encoding='utf-8') as fp:
+            for title in failed_list:
+                fp.write(str(title) + "\n")
